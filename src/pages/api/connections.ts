bf74db7eac1fa1f416e625ae7250b7ec48c00040
Darren Zhang <69101968+darzhang@users.noreply.github.com>
2024-03-04 03:12:59 +1100

    modify connections api to include the POST method to create new connection

--- src/pages/api/connections.ts
+++ src/pages/api/connections.ts
@@ -3,19 +3,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+type CreateConnectionBodyType = {
+  title: string;
+  stops: { id: string }[];
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  // User Session Guard
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
   switch (req.method) {
     case "GET":
       try {
-        // User Session Guard
-        const session = await getServerSession(req, res, authOptions);
-        if (!session) {
-          return res.status(401).json({ error: "Unauthorized" });
-        }
-
         // Get the user connections
         const user = await prisma.user.findUnique({
           where: { email: session.user.email },
@@ -35,8 +40,44 @@ export default async function handler(
           .status(500)
           .json({ error: "Failed to get user's connections." });
       }
+
+    case "POST":
+      try {
+        // Create a new connection
+        const { title, stops }: CreateConnectionBodyType = req.body;
+        const newConnection = await prisma.connection.create({
+          data: {
+            title,
+            user: { connect: { email: session.user.email } },
+            stops: { connect: stops.map((stop) => ({ id: Number(stop.id) })) },
+          },
+        });
+
+        // Throw Error if new connection not created
+        if (!newConnection) {
+          return res
+            .status(400)
+            .json({ error: "Failed to create a new connection." });
+        }
+
+        return res.status(201).json(newConnection);
+      } catch (error: any) {
+        console.error(error);
+
+        // Throw Duplicate Error
+        if (error.code === "P2002") {
+          return res
+            .status(400)
+            .json({ error: "Connection with the same title already exists." });
+        }
+
+        return res
+          .status(500)
+          .json({ error: "Failed to create a new connection." });
+      }
+
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["GET", "POST"]);
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
