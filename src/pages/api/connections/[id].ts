ff83188bd1f5117a193abb4007758a0320ffde3e
Darren Zhang <69101968+darzhang@users.noreply.github.com>
2024-03-04 04:31:25 +1100

    modify the get connections by id to return only the next 3 departure times

--- src/pages/api/connections/[id].ts
+++ src/pages/api/connections/[id].ts
@@ -29,6 +29,35 @@ export default async function handler(
           return res.status(404).json({ error: "Connection not found" });
         }
 
+        // Get the current time in 'HH:mm:ss' format in Melbourne Timezone
+        const melbourneOffset = 11; // UTC+11 for now (ease of use, should be dynamic in production)
+        const nowUtc = new Date();
+        const nowMelbourne = new Date(
+          nowUtc.getTime() + melbourneOffset * 60 * 60 * 1000,
+        );
+        const nowMelbourneTime = nowMelbourne.toISOString().substring(11, 19);
+
+        connection.stops.forEach((stop) => {
+          // Sort the departures by departureTime
+          stop.departures.sort((a, b) =>
+            a.departureTime.localeCompare(b.departureTime),
+          );
+
+          // Only take the future departure
+          const futureDepartures = stop.departures.filter(
+            (departure) => departure.departureTime > nowMelbourneTime,
+          );
+
+          // If there are less than 3 future departures today, take the earliest ones from the list to make up the numbers
+          if (futureDepartures.length < 3) {
+            const needed = 3 - futureDepartures.length;
+            for (let i = 0; i < needed; i++) {
+              if (stop.departures[i]) futureDepartures.push(stop.departures[i]);
+            }
+          }
+          stop.departures = futureDepartures.slice(0, 3);
+        });
+
         return res.status(200).json(connection);
       } catch (error: any) {
         console.error(error);
